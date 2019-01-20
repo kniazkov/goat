5f362161bfaa90ca399c8821de676a00f0ae6636
Ivan Kniazkov <iknyaz@yandex.ru>
2019-01-20 11:44:43 +0300

    Common model

---
+++ src/model/object.cpp
@@ -0,0 +1,103 @@
+/*
+
+Copyright (C) 17..19 Ivan Kniazkov
+
+This file is part of interpreter of programming language
+codenamed "Goat" ("Goat interpreter").
+
+Goat interpreter is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Goat interpreter is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with Goat interpreter.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "object.h"
+#include <sstream>
+
+namespace g0at
+{
+    namespace model
+    {
+        /* 
+            Generic object
+        */
+
+        object::object()
+        {
+        }
+
+        object::~object()
+        {
+        }
+
+        object_type object::get_type() const
+        {
+            return GENERIC;
+        }
+
+        bool object::less(const object *obj) const
+        {
+            return this < obj;
+        }
+
+        std::wstring object::to_string() const
+        {
+            std::wstringstream stream;
+            stream << '{';
+
+            int k = 0;
+            for (auto pair : objects)
+            {
+                if (k)
+                    stream << ',';
+                k++;
+                stream << pair.first->to_string() << ':' << pair.second.to_string();
+            }
+
+            stream << '}';
+            return stream.str();
+        }
+
+        /* 
+            Generic handler
+        */
+
+        handler::~handler()
+        {
+        }
+
+        class generic_handler : public handler
+        {
+        protected:
+            generic_handler()
+            {
+            }
+
+        public:
+            static handler *get_instance()
+            {
+                static generic_handler instance;
+                return &instance;
+            }
+
+            std::wstring to_string(const variable *var) const override
+            {
+                return var->data.obj->to_string();
+            }
+        };
+
+        handler *handler::get_generic_instance()
+        {
+            return generic_handler::get_instance();
+        }
+    };
+};
---
+++ src/model/object.h
@@ -0,0 +1,115 @@
+/*
+
+Copyright (C) 17..19 Ivan Kniazkov
+
+This file is part of interpreter of programming language
+codenamed "Goat" ("Goat interpreter").
+
+Goat interpreter is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Goat interpreter is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with Goat interpreter.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#pragma once
+
+#include <map>
+#include <vector>
+#include <string>
+
+namespace g0at
+{
+    namespace model
+    {
+        class object;
+        class handler;
+
+        enum object_type
+        {
+            KEY,
+            GENERIC,
+            STRING
+        };
+
+        class object_comparator
+        {
+        public:
+            inline bool operator() (const object *a, const object *b) const;
+        };
+
+        class variable
+        {
+        public:
+            variable()
+            {
+                hndl = nullptr;
+                data.obj = nullptr;
+            }
+
+            inline void set_object(object *obj);
+            inline std::wstring to_string() const;
+
+            handler *hndl;
+            union
+            {
+                object *obj;
+            } data;
+        };
+
+        class object
+        {
+        public:
+            object();
+            virtual ~object();
+            virtual object_type get_type() const;
+            virtual bool less(const object *obj) const;
+            virtual std::wstring to_string() const;
+
+        protected:
+            std::map<object*, variable, object_comparator> objects;
+            std::vector<object*> proto;
+        };
+
+        class handler
+        {
+        public:
+            virtual ~handler();
+            static handler *get_generic_instance();
+            virtual std::wstring to_string(const variable *var) const = 0;
+        };
+
+        bool object_comparator::operator ()(const object *a, const object *b) const
+        {
+            if (a == b)
+                return false;
+
+            object_type x = a->get_type();
+            object_type y = b->get_type();
+
+            if (x == y)
+                return a->less(b);
+
+            return x < y;
+        }
+
+        void variable::set_object(object *obj)
+        {
+            hndl = handler::get_generic_instance();
+            data.obj = obj;
+        }
+
+        std::wstring variable::to_string() const
+        {
+            return hndl->to_string(this);
+        }
+    };
+};
