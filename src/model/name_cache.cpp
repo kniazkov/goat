5893def0a0c448d5883d297a967e779648d615aa
Ivan Kniazkov <iknyaz@yandex.ru>
2019-01-29 18:57:22 +0300

    Add cache for name (identifiers)

---
+++ src/model/name_cache.cpp
@@ -0,0 +1,45 @@
+/*
+
+Copyright (C) 17..19 Ivan Kniazkov
+
+This file is part of interpreter of programming language
+codenamed "Goat" ("Goat interpreter").
+
+Goat interpreter is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Goat interpreter is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with Goat interpreter.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "name_cache.h"
+
+namespace g0at
+{
+    namespace model
+    {
+        name_cache::name_cache()
+        {
+        }
+
+        int name_cache::get_id(std::wstring name)
+        {
+            auto iter = map.find(name);
+            if (iter != map.end())
+                return iter->second;
+
+            int id = (int)vector.size();
+            map.insert(std::pair<std::wstring, int>(name, id));
+            vector.push_back(name);
+            return id;
+        }
+    };
+};
