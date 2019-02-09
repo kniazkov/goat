950d9dbf46ce48d41499bab52eda4018b8916c65
Ivan Kniazkov <iknyaz@yandex.ru>
2019-02-09 13:04:17 +0300

    + test

--- create_test.sh
+++ create_test.sh
@@ -1,14 +1,20 @@
 if [ -n "$1" ]
 then
-    echo Create test \'$1\'...
-    mkdir test/$1
-    cp program.goat test/$1
-    python test.py build/goat -select=$1
-    [ -f test/$1/output_act.txt ] &&  mv test/$1/output_act.txt test/$1/output.txt
-    [ -f test/$1/error_act.txt ] &&  mv test/$1/error_act.txt test/$1/error.txt
-    python test.py build/goat -select=$1
-    git add test/$1/*
-    echo Done.
+    if [ -d test/$1 ]
+    then
+        echo Test \'$1\' already exists!
+    else
+        echo Create test \'$1\'...
+        mkdir test/$1
+        cp program.goat test/$1
+        python test.py build/goat -select=$1
+        [ -f test/$1/output_act.txt ] &&  mv test/$1/output_act.txt test/$1/output.txt
+        [ -f test/$1/error_act.txt ] &&  mv test/$1/error_act.txt test/$1/error.txt
+        python test.py build/goat -select=$1
+        git add test/$1/*
+        git commit -am "+ test"
+        echo Done.
+    fi
 else
     echo "Usage:"
     echo "    ./create_test.sh <test_name>"
