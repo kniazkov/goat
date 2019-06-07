b618e61214d9457dc70b92925c37c6464b59bb58
Ivan Kniazkov <iknyaz@yandex.ru>
2019-06-07 15:54:09 +0300

    Added release build

--- test_all.sh
+++ test_all.sh
@@ -1,5 +1,5 @@
 ./build.sh
-find ./test -name "output_act.txt" | xargs rm
-find ./test -name "error_act.txt" | xargs rm
+find ./test -name "output_act.txt" | xargs rm -f
+find ./test -name "error_act.txt" | xargs rm -f
 echo
 python ./test.py build/goat -all -failed
