cae4975e1d74b63f6f676d474791a28058b25909
ivan <iknyaz@yandex.ru>
2019-07-05 16:57:42 +0000

    testing release build with debug option also

--- release.sh
+++ release.sh
@@ -13,3 +13,5 @@ find ./test -name "output_act.txt" | xargs rm -f
 find ./test -name "error_act.txt" | xargs rm -f
 echo
 python3 ./test.py build/goat -all -failed
+python3 ./test.py build/goat -all -failed -debug
+
