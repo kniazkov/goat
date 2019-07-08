1d34087c37b7bc7106c0d3e57cc98682eb51fa13
Ivan Kniazkov <iknyaz@yandex.ru>
2019-07-08 13:39:17 +0300

    enable --gc=debug option

--- test_all.sh
+++ test_all.sh
@@ -3,4 +3,4 @@ find ./test -name "output_act.txt" | xargs rm -f
 find ./test -name "error_act.txt" | xargs rm -f
 echo
 python3 ./test.py build/goat -all -failed
-python3 ./test.py build/goat -debug -all -failed
+python3 ./test.py build/goat -debug -failed
