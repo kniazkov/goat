c8afb0e6ee3511e3dbea332f2e7420723d09f0f6
Ivan Kniazkov <ivan@kniazkov.com>
2021-04-26 16:08:37 +0300

    New simple build system

--- build.cmd
+++ build.cmd
@@ -1,7 +1,1 @@
-if exist goat.exe del goat.exe
-if not exist build mkdir build
-cd build
-cmake ..\src -G "MinGW Makefiles"
-mingw32-make -j4
-cd ..
-copy build\goat.exe .
+g++ src/*.cpp src/code/*.cpp src/compiler/analyzer/*.cpp src/compiler/ast/*.cpp src/compiler/codegen/*.cpp src/compiler/common/*.cpp src/compiler/parser/*.cpp src/compiler/pt/*.cpp src/compiler/pt/root_scope/*.cpp  src/compiler/scanner/*.cpp src/compiler/source/*.cpp src/global/*.cpp src/launcher/*.cpp src/lib/*.cpp src/model/*.cpp src/model/built_in/*.cpp src/resource/*.cpp src/vm/*.cpp -Isrc -ldl -lpthread -g -o goat.exe
